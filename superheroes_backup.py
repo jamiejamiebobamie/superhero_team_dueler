2bc54d32c767dca1efb9f487e57f3eaa3a0e863f
jamiejamiebobamie <jmccrory@vt.edu>
2018-09-18 20:38:21 -0700

    saved over backup

--- superheroes_backup.py
+++ superheroes_backup.py
@@ -47,7 +47,7 @@ class Hero:
         else: return 0
 
     def take_damage(self, damage_amt):
-        self.health -= damage_amt
+        self.health += damage_amt
         if self.health < 1:
             self.deaths += 1
 
@@ -105,8 +105,12 @@ class Weapon(Ability):
     note: weapon's have a wider spectrum of possible values, given the lower lower bounds.
 
     end_Docstringtest!"""
+
     def attack(self):
         return random.randint(0, self.attack_strength)
+
+    def defend(self):
+        return random.randint(0, self.attack_strength // 2)
 
 class Armor:
     """One method:
@@ -175,18 +179,26 @@ class Team:
             myHeroes.append(hero.name)
         return myHeroes
 
+    def view_all_heroes_stats(self):
+        myHeroes = []
+        abilities = []
+        for hero in self.heroes:
+            for ability in hero.abilities:
+                abilities.append((ability.name, ability.attack_strength))
+            myHeroes.append((hero.name, hero.health, abilities))
+        return myHeroes
+
     def attack(self, other_team):
         totalAttack = 0
         for hero in self.heroes:
             totalAttack += hero.attack()
-        other_team.defend(totalAttack)
-
-        enemiesDead = 0
-        for enemy in other_team.heroes:
-            enemiesDead += enemy.deaths
+        enemiesDead = other_team.defend(totalAttack)
 
         for hero in self.heroes:
             hero.add_kill(enemiesDead)
+
+        return totalAttack
+
 
     def defend(self, damage_amt):
         deadHeroes = 0
@@ -213,13 +225,14 @@ class Team:
             hero.health = hero.start_health
 
     def stats(self):
+        stats_array = []
         for hero in self.heroes:
             if hero.deaths != 0:
                 ratio = hero.kills // hero.deaths
-                print(str(hero.name) + " killed " + str(hero.kills) + " hero(es) and died " + str(hero.deaths) + " time(s). THEIR KILLS TO DEATHS RATIO WAS " + str(ratio) + ".")
-        else:
-            for hero in self.heroes:
-                print(str(hero.name) + " killed " + str(hero.kills) + " hero(es) and never died.")
+                stats_array.append((str(hero.name) + " has " + str(hero.health) + " health, killed " + str(hero.kills) + " hero(es) and died " + str(hero.deaths) + " time(s). THEIR KILLS TO DEATHS RATIO WAS " + str(ratio) + "."))
+            else:
+                stats_array.append((str(hero.name) + " has " + str(hero.health) + " health, killed " + str(hero.kills) + " hero(es) and never died."))
+        return stats_array
 
     def update_kills(self):
         totalTeamKills = 0
@@ -227,13 +240,11 @@ class Team:
             totalTeamKills += hero.kills
         return totalTeamKills
 
-        totalteamKills = totalTeamKills // len(self.heroes) #this is definitely wrong...
-
     def deadHeroes(self):
         deadHeroes = 0
         for hero in self.heroes:
             deadHeroes += hero.deaths
-        if deadHeroes == len(self.heroes):
+        if deadHeroes >= len(self.heroes):
             return True
         else:
             return False
@@ -255,168 +266,141 @@ class Arena:
         self.team_two = None
 
     def build_team_one(self):
-        heroBool = True
-        powerBool = True
         hero1 = ""
         ability1 = ""
         heroDict = {}
-        counter = 0
-        print("Hello, welcome to hell.\n")
-        teamName1 = input("What is your first team of super hero's name?\n\n")
+        heroCounter = 0
+        powerCounter = 0
+        yes = ["yeah", "y", "yes", "ye"]
+        print("\nHello, welcome to hell.\n")
+        teamName1 = input("What is your FIRST TEAM of super hero's NAME?\n\n")
         self.team_one = Team(teamName1)
-        print("Very cool. \n")
-        while heroBool == True and counter < 5:
-            if hero1 == "x":
-                heroBool = False
-            else:
-                heroDict[counter] = Hero(input("Please enter the name of a super-hero on team " + str(self.team_one.name) + ". When you are finished adding heroes to your party, enter the letter 'x'.\n"))
-                self.team_one.add_hero(heroDict[counter])
+        print("\nVery cool. \n")
+        while len(self.team_one.heroes) < 2:
+                heroDict[heroCounter] = Hero(input("Please enter the name of a superhero on " + str(self.team_one.name) + ". \n \n"))
+                self.team_one.add_hero(heroDict[heroCounter])
+                print("\n\nYour team: ")
                 print(self.team_one.view_all_heroes())
-                print((powerBool, ability1, counter))
-                powerBool = True
-                while powerBool == True:
+                print("\nEach hero can have three powers. \nThe strength of each power is randomly generated \nbetween weak (strength: <10) and strong (stength: 100+).\n")
+                powerCounter = 0
+                while powerCounter < 2:
                     if ability1 == "x":
                         ability1 = ""
-                        counter += 1
-                        powerBool = False
+                        heroCounter += 1
                     else:
-                        print(heroDict[counter].name)
-                        if len(heroDict[counter].abilities) > 0:
-                            print(heroDict[counter].listAbilities())
-                        ability1 = input("What's your superhero's superpower called? Type 'x' to stop.\n")
-                        abilityability = Ability(ability1, random.randint(0,100))
-                        heroDict[counter].add_ability(abilityability)
-
-
-
+                        ability1 = input("\nWhat's " + heroDict[heroCounter].name + "'s superpower # " + str(powerCounter + 1) + " called? \n\n")
+                        abilityability = Ability(ability1, (powerCounter + 1) * random.randint(1,60))
+                        heroDict[heroCounter].add_ability(abilityability)
+                        print("")
+                        powerCounter += 1
+                print("Does your hero have a weapon?")
+                answer = str.lower(input("Enter yes or no.\n\n"))
+                if answer in yes:
+                    weapon = input("\n\nWhat's your weapon?\n\n")
+                    weapon1 = Weapon(str(weapon), random.randint(1,100))
+                    heroDict[heroCounter].add_ability(weapon1)
+                else:
+                    print("\n\nYour hero has no weapon.\n\n")
+                print("\n\nDoes your hero wear armor.")
+                answer = str.lower(input("Enter yes or no.\n\n"))
+                if answer in yes:
+                    armor = input("\n\nWhat's the armor your hero is wearing?\n\n")
+                    armor1 = Weapon(str(armor), random.randint(1,50))
+                    heroDict[heroCounter].add_armor(armor1)
+                else:
+                    print("Your hero has no armor.")
+                print("Your abilities are: \n\n")
+                print(heroDict[heroCounter].listAbilities())
+                if len(heroDict[heroCounter].armors) > 0:
+                    print("Your armor is: \n\n")
+                    print(armor)
 
     def build_team_two(self):
-        heroBool = True
-        powerBool = True
-        hero2 = ""
-        ability2 = ""
+        hero1 = ""
+        ability1 = ""
         heroDict = {}
-        counter = 0
-        print("Way to go!\n\n")
-        teamName2 = input("What is your second team of super hero's name?\n\n")
+        heroCounter = 0
+        powerCounter = 0
+        yes = ["yeah", "y", "yes", "ye"]
+        print("\nHello, welcome to hell.\n")
+        teamName2 = input("What is your SECOND TEAM of super hero's NAME?\n\n")
         self.team_two = Team(teamName2)
-        print("Very cool. \n")
-        while heroBool == True and counter < 5:
-            if hero2 == "x":
-                heroBool = False
-            else:
-                heroDict[counter] = Hero(input("Please enter the name of a super-hero on team " + str(self.team_two.name) + ". When you are finished adding heroes to your party, enter the letter 'x'.\n"))
-                self.team_two.add_hero(heroDict[counter])
+        print("\nVery cool. \n")
+        while len(self.team_two.heroes) < 2:
+                heroDict[heroCounter] = Hero(input("Please enter the name of a superhero on " + str(self.team_two.name) + ". \n \n"))
+                self.team_two.add_hero(heroDict[heroCounter])
+                print("\n\nYour team: ")
                 print(self.team_two.view_all_heroes())
-                print((powerBool, ability2, counter))
-                powerBool = True
-                while powerBool == True:
-                    if ability2 == "x":
-                        ability2 = ""
-                        counter += 1
-                        powerBool = False
+                print("\nEach hero can have three powers. \nThe strength of each power is randomly generated \nbetween weak (strength: <10) and strong (stength: 100+).\n")
+                powerCounter = 0
+                while powerCounter < 2:
+                    if ability1 == "x":
+                        ability1 = ""
+                        heroCounter += 1
                     else:
-                        print(heroDict[counter].name)
-                        if len(heroDict[counter].abilities) > 0:
-                            print(heroDict[counter].listAbilities())
-                        ability2 = input("What's your superhero's superpower called? Type 'x' to stop.\n")
-                        abilityability = Ability(ability2, random.randint(0,100))
-                        heroDict[counter].add_ability(abilityability)
-
+                        ability1 = input("\nWhat's " + heroDict[heroCounter].name + "'s superpower # " + str(powerCounter + 1) + " called? \n\n")
+                        abilityability = Ability(ability1, (powerCounter + 1) * random.randint(1,60))
+                        heroDict[heroCounter].add_ability(abilityability)
+                        print("")
+                        powerCounter += 1
+                print("Does your hero have a weapon?")
+                answer = str.lower(input("Enter yes or no.\n\n"))
+                if answer in yes:
+                    weapon = input("\n\nWhat's your weapon?\n\n")
+                    weapon1 = Weapon(str(weapon), random.randint(1,100))
+                    heroDict[heroCounter].add_ability(weapon1)
+                else:
+                    print("\n\nYour hero has no weapon.\n\n")
+                print("\n\nDoes your hero wear armor.")
+                answer = str.lower(input("Enter yes or no.\n\n"))
+                if answer in yes:
+                    armor = input("\n\nWhat's the armor your hero is wearing?\n\n")
+                    armor1 = Weapon(str(armor), random.randint(1,50))
+                    heroDict[heroCounter].add_armor(armor1)
+                else:
+                    print("Your hero has no armor.")
+                print("Your abilities are: \n\n")
+                print(heroDict[heroCounter].listAbilities())
+                if len(heroDict[heroCounter].armors) > 0:
+                    print("Your armor is: \n")
+                    print(armor)
+                    print(" ")
 
     def team_battle(self):
-        build_team_one()
-        build_team_two()
-        while self.team_one.deadHeroes() == False and self.team_two.deadHeroes() == False:
-            pass
-            #battle
+        gameover1 = False
+        gameover2 = False
+        self.build_team_one()
+        self.build_team_two()
+        print("Your teams are: ")
+        print(self.team_one.view_all_heroes_stats())
+        print(self.team_two.view_all_heroes_stats())
+        x = random.randint(0,1)
+        print("\nWho strikes first??")
+        if x == True:
+            print("\nTeam one got the drop on team two!!! Careful team two!")
+            while gameover1 != True and gameover2 != True:
+                self.team_one.attack(self.team_two)
+                gameover1 = self.team_two.deadHeroes()
+                gameover2 = self.team_one.deadHeroes()
+                self.team_two.attack(self.team_one)
+                gameover1 = self.team_two.deadHeroes()
+                gameover2 = self.team_one.deadHeroes()
+        else:
+            print("Team two got the drop on team one!!! Careful team one!")
+            while gameover1 != True and gameover2 != True:
+                self.team_two.attack(self.team_one)
+                gameover1 = self.team_one.deadHeroes()
+                gameover2 = self.team_two.deadHeroes()
+                self.team_one.attack(self.team_two)
+                gameover1 = self.team_one.deadHeroes()
+                gameover2 = self.team_two.deadHeroes()
 
     def show_stats(self):
-        teams = list(self.team_one, self.team_two)
-        for team in teams:
-            team.stats()
-
-
-
-"""myHero = Hero("Shrek")
-print(myHero.name)
-print(myHero.__doc__)
-loadedFart = Ability("loaded fart", 30)
-myHero.add_ability(loadedFart)
-poop = Ability("poop", 40)
-myHero.add_ability(poop)
-print(myHero.health)
-myHero.take_damage(99)
-print(myHero.defend())
-print(myHero.health)
-myHero.take_damage(99)
-print(myHero.health)
-print(myHero.defend())
-print(myHero.attack())
-myHero.add_kill(3)
-print(myHero.kills)
-
-scimitar = Weapon("scimitar", 100)
-
-myHero.add_ability(scimitar)
-print(myHero.listAbilities())
-print(myHero.attack())
-
-shield = Armor("shield", 50)
-myHero.add_armor(shield)
-print(myHero.listArmors())
-print(myHero.defend())
-print(len(myHero.armors))
-print(myHero.health)
-print(myHero.defend())
-
-Team1 = Team("myTeam")
-Team2 = Team("otherTeam")
-Team2.add_hero(myHero)
-print(Team2.view_all_heroes())
-print(Team1.name)
-
-Team1.add_hero(myHero)
-print(Team1.view_all_heroes())
-Team1.remove_hero(myHero)
-print(Team1.view_all_heroes())
-Team1.add_hero(myHero)
-print(Team1.view_all_heroes())
-
-print(myHero.health)
-
-Team2.revive_heroes()
-
-print(myHero.health)
-
-Team1.stats()
-
-myHero.kills = 5
-myHero.deaths = 0
-
-print(Team1.update_kills())
-
-print(myHero.kills)
-Team1.stats()
-Team2.stats()
-myHero.deaths = 1
-
-print(Team1.deadHeroes())
-
-myHero.kills = 0
-
-print(Team1.update_kills())
-
-print(str(Team1.find_hero("Shrek").name))
-
-Team1.attack(Team2)
-
-
-hello = Ability("hello", 2)
-Team1.find_hero("Shrek").add_ability(hello)
-print(Team1.find_hero("Shrek").listAbilities())
-"""
+        print("\n\nTEAM ONE STATS:")
+        print(self.team_one.stats())
+        print("\nTEAM TWO STATS:")
+        print(self.team_two.stats())
 
 myArena = Arena()
-
 myArena.team_battle()
+myArena.show_stats()
